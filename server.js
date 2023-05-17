dbaf941b14aa719eea18439896c096ef65a442bd
syltur <sylwia.tur99@gmail.com>
2023-05-17 13:19:14 +0200

    connect to atlas

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 const express = require('express');
 const app = express();
-// const { v4: uuidv4 } = require('uuid');
-// const db = require('./db') ;
 const cors = require('cors');
 const path = require('path');
 const testimonialsRoutes = require('./routes/testimonials.routes');
@@ -10,11 +8,27 @@ const seatsRoutes = require('./routes/seats.routes');
 const socket = require('socket.io');
 const mongoose = require('mongoose');
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '/client/build')));
+const MONGO_USERNAME = 'john-doe';
+const MONGO_PASSWORD = 'SCRAM';
+const MONGO_HOSTNAME = 'cluster0.dutnfbz.mongodb.net';
+const MONGO_DATABASE = 'company';
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(`mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}/${MONGO_DATABASE}`, options);
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Connected to the database');
+});
 
 app.use(cors());
-app.use(express.urlencoded({ extends: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -34,16 +48,6 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
 
-mongoose.connect('mongodb+srv://john-doe:<password>@cluster0.dutnfbz.mongodb.net/?retryWrites=true&w=majority', {
-  useNewUrlParser: true,
-});
-const db = mongoose.connection;
-
-db.once('open', () => {
-  console.log('Connected to the database');
-});
-db.on('error', (err) => console.log('Error ' + err));
-
 const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
@@ -53,48 +57,3 @@ const io = socket(server);
 io.on('connection', (socket) => {
   console.log('new socket!', socket.id);
 });
-
-// app.get('/testimonials/random', (req, res) => {
-//   const random = Math.floor(Math.random() * db.testimonials.length);
-//   res.json(db.testimonials[random]);
-// });
-
-// app.get('/testimonials', (req, res) => {
-//   res.json(db.testimonials);
-// });
-
-// app.get('/testimonials/:id', (req, res) => {
-//   res.json(db.testimonials.filter((element) => element.id == req.params.id));
-// });
-
-// app.post('/testimonials', (req, res) => {
-//   db.testimonials.push({
-//     id: uuidv4(),
-//     author: req.body.author,
-//     text: req.body.text,
-//   });
-
-//   res.json({ message: 'OK' });
-// });
-
-// app.put('/testimonials/:id', (req, res) => {
-//   db.testimonials.map((element) => {
-//     if (element.id == req.params.id) {
-//       element.author = req.body.author;
-//       element.text = req.body.text;
-//     } else {
-//       return element;
-//     }
-//   });
-
-//   res.json({ message: 'OK' });
-// });
-
-// app.delete('/testimonials/:id', (req, res) => {
-//   const index = db.testimonials.findIndex(
-//     (element) => element.id == req.params.id
-//   );
-//   db.testimonials.splice(index, 1);
-
-//   res.json({ message: 'OK' });
-// });
