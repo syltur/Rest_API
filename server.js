de464b656b102621a52228c5c2a53105e6543801
syltur <sylwia.tur99@gmail.com>
2023-05-20 12:54:02 +0200

    add tests

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+// const { v4: uuidv4 } = require('uuid');
+// const db = require('./db') ;
 const cors = require('cors');
 const path = require('path');
 const testimonialsRoutes = require('./routes/testimonials.routes');
@@ -8,22 +10,11 @@ const seatsRoutes = require('./routes/seats.routes');
 const socket = require('socket.io');
 const mongoose = require('mongoose');
 
-const options = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-
-mongoose.connect('mongodb+srv://john-doe:SCRAM@cluster0.dutnfbz.mongodb.net/?retryWrites=true&w=majority', options);
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Connected to the database');
-});
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use(cors());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extends: false }));
 app.use(express.json());
 
 app.use((req, res, next) => {
@@ -43,8 +34,22 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' });
 });
 
+const NODE_ENV = process.env.NODE_ENV;
+let dbUri = '';
+
+if (NODE_ENV === 'test') {
+  dbUri = 'mongodb://localhost:27017/NewWaveDBtest';
+} else {
+  dbUri =
+  'mongodb+srv://john-doe:SCRAM@cluster0.dutnfbz.mongodb.net/?retryWrites=true&w=majority';
+}
+mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.once('open', () => {
+});
+db.on('error', (err) => console.log('Error ' + err));
+
 const server = app.listen(process.env.PORT || 8000, () => {
-  console.log('Server is running on port: 8000');
 });
 
 const io = socket(server);
@@ -52,3 +57,5 @@ const io = socket(server);
 io.on('connection', (socket) => {
   console.log('new socket!', socket.id);
 });
+
+module.exports = server;
